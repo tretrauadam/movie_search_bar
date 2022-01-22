264f27106c968654b52148cda7d81359db1aa9c4
adamzermane <adamzermane92@yahoo.com>
2022-01-22 13:32:50 +0100

    search bar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      sql_query = " \
-        movies.title @@ :query \
-        OR movies.synopsis @@ :query \
-        OR directors.first_name @@ :query \
-        OR directors.last_name @@ :query \
-      "
-      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    @movies = Movie.search_by_title_and_synopsis(params[:query])
     else
-      @movies = Movie.all
+    @movies = Movie.all
     end
   end
-end 
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
 class Movie < ApplicationRecord
   belongs_to :director
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_synopsis,
+    against: [ :title, :synopsis ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
