265521824d49462f422c697fbb8fc95c22e8138e
adamzermane <adamzermane92@yahoo.com>
2022-01-22 11:13:24 +0100

    posgresql search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,15 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      @movies = Movie.where(title: params[:query])
+      sql_query = " \
+        movies.title @@ :query \
+        OR movies.synopsis @@ :query \
+        OR directors.first_name @@ :query \
+        OR directors.last_name @@ :query \
+      "
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
     else
       @movies = Movie.all
     end
   end
-end
+end 
