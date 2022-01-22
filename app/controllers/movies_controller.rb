698cac97876ac5ea6fdd870219410309fd8b0117
adamzermane <adamzermane92@yahoo.com>
2022-01-22 14:29:56 +0100

    search bar multitask

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-    @movies = Movie.search_by_title_and_synopsis(params[:query])
+      @movies = Movie.search_by_title_and_synopsis(params[:query])
     else
-    @movies = Movie.all
+      @movies = Movie.all
     end
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,13 @@
 class Movie < ApplicationRecord
   belongs_to :director
   include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
+  include PgSearch::Model
   pg_search_scope :search_by_title_and_synopsis,
     against: [ :title, :synopsis ],
+    associated_against: {
+      director: [ :first_name, :last_name]
+    },
     using: {
       tsearch: { prefix: true }
     }
